7e310b31f378d009d3c0ceef215a17c013fee734
Michael Willingham <michael@willingham.io>
2026-01-24 16:37:58 -0600

    Fix RuboCop style violations and update test workflow to macos-latest

--- Casks/netlogo.rb
+++ Casks/netlogo.rb
@@ -1,6 +1,6 @@
 cask "netlogo" do
   arch arm: "aarch64", intel: "x86_64"
-  
+
   version "7.0.3"
   sha256 arm:   "14fb4870415fe78e7391641a62aaab96e570b4531ac3cd82d8d16ad8f60a24ca",
          intel: "79ecc61c231559681f1d2f0f79943956ae681c538fc65fef1c781e483ae6b5e6"
