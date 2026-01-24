5ea56d74dd67aa1857626c0e78b6cfa6d021ac6f
Michael Willingham <michael@willingham.io>
2026-01-24 16:20:07 -0600

    Add verified parameter to NetLogo cask URL

--- Casks/netlogo.rb
+++ Casks/netlogo.rb
@@ -5,7 +5,8 @@ cask "netlogo" do
   sha256 arm:   "14fb4870415fe78e7391641a62aaab96e570b4531ac3cd82d8d16ad8f60a24ca",
          intel: "79ecc61c231559681f1d2f0f79943956ae681c538fc65fef1c781e483ae6b5e6"
 
-  url "https://github.com/NetLogo/NetLogo/releases/download/v#{version}/NetLogo-#{version}-#{arch}.dmg"
+  url "https://github.com/NetLogo/NetLogo/releases/download/v#{version}/NetLogo-#{version}-#{arch}.dmg",
+      verified: "github.com/NetLogo/NetLogo/"
   name "NetLogo"
   desc "Multi-agent programmable modeling environment"
   homepage "https://www.netlogo.org/"
