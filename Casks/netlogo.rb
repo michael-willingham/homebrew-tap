c10aebc51ccc59ea5fc5bcd9d742a323ef9025a5
Michael Willingham <michael@willingham.io>
2026-01-24 13:01:10 -0600

    Add NetLogo cask with GitHub release URLs and auto-update workflow

---
+++ Casks/netlogo.rb
@@ -0,0 +1,24 @@
+cask "netlogo" do
+  arch arm: "aarch64", intel: "x86_64"
+  
+  version "7.0.3"
+  sha256 arm:   "14fb4870415fe78e7391641a62aaab96e570b4531ac3cd82d8d16ad8f60a24ca",
+         intel: "79ecc61c231559681f1d2f0f79943956ae681c538fc65fef1c781e483ae6b5e6"
+
+  url "https://github.com/NetLogo/NetLogo/releases/download/v#{version}/NetLogo-#{version}-#{arch}.dmg"
+  name "NetLogo"
+  desc "Multi-agent programmable modeling environment"
+  homepage "https://www.netlogo.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  suite "NetLogo #{version}"
+
+  zap trash: [
+    "~/Library/Preferences/org.nlogo.NetLogo.plist",
+    "~/Library/Saved Application State/org.nlogo.NetLogo.savedState",
+  ]
+end
